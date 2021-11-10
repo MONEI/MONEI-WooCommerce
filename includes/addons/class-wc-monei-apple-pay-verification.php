008a795a7fb7c6c2d473956958969089914ee2fd
manuelRod <manuel.rodriguez.rosado@gmail.com>
2021-11-10 18:38:43 +0100

    dont expose file if setting not active

--- includes/addons/class-wc-monei-apple-pay-verification.php
+++ includes/addons/class-wc-monei-apple-pay-verification.php
@@ -12,17 +12,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 class WC_Monei_Addons_Apple_Pay_Verification {
 
 	const DOMAIN_ASSOCIATION_FILE_NAME = 'apple-developer-merchantid-domain-association';
-	const DOMAIN_ASSOCIATION_DIR  = '.well-known';
+	const DOMAIN_ASSOCIATION_DIR       = '.well-known';
 
 	public function __construct() {
-		add_action( 'parse_request', [ $this, 'expose_on_domain_association_request' ], 1 );
+		/**
+		 * If not active, we don't expose file.
+		 */
+		if ( 'yes' !== monei_get_settings( 'apple_google_pay' ) ) {
+			return;
+		}
+		add_action( 'parse_request', array( $this, 'expose_on_domain_association_request' ), 1 );
 	}
 
 	/**
 	 * Expose DOMAIN_ASSOCIATION_FILE_NAME on https://example.com/.well-known/apple-developer-merchantid-domain-association request.
 	 * @param $wp
 	 */
-	function expose_on_domain_association_request( $wp ) {
+	public function expose_on_domain_association_request( $wp ) {
 		if ( isset( $wp->request ) && ( self::DOMAIN_ASSOCIATION_DIR . '/' . self::DOMAIN_ASSOCIATION_FILE_NAME ) === $wp->request ) {
 			$path = WC_Monei()->plugin_path() . '/' . self::DOMAIN_ASSOCIATION_FILE_NAME;
 			header( 'Content-Type: text/plain;charset=utf-8' );
