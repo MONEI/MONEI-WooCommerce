dcd3d3ce9e7523e22319431b2f076c7452a8a08a
Gabriel Reguly <github@ppgr.com.br>
2024-04-03 18:34:56 -0300

    update version to 6.0.0
    Also updates copyright years

--- class-woocommerce-gateway-monei.php
+++ class-woocommerce-gateway-monei.php
@@ -5,7 +5,7 @@
  * @author   MONEI
  * @category Core
  * @package  Woocommerce_Gateway_Monei
- * @version  5.7.2
+ * @version  6.0.0
  */
 if ( ! class_exists( 'Woocommerce_Gateway_Monei' ) ) :
 
@@ -16,7 +16,7 @@ if ( ! class_exists( 'Woocommerce_Gateway_Monei' ) ) :
 		 *
 		 * @var string
 		 */
-		public $version = '5.7.2';
+		public $version = '6.0.0';
 
 		/**
 		 * The single instance of the class.
