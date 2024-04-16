3bbd858ea48cb292dfb74f601f754e795fca9fed
Dmitriy <dn@monei.com>
2024-04-16 16:11:08 +0200

    chore: release v5.8.6

--- class-woocommerce-gateway-monei.php
+++ class-woocommerce-gateway-monei.php
@@ -5,7 +5,7 @@
  * @author   MONEI
  * @category Core
  * @package  Woocommerce_Gateway_Monei
- * @version  5.8.5
+ * @version  5.8.6
  */
 if ( ! class_exists( 'Woocommerce_Gateway_Monei' ) ) :
 
@@ -16,7 +16,7 @@ if ( ! class_exists( 'Woocommerce_Gateway_Monei' ) ) :
 		 *
 		 * @var string
 		 */
-		public $version = '5.8.5';
+		public $version = '5.8.6';
 
 		/**
 		 * The single instance of the class.
