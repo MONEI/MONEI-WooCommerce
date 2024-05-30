0aa738ba14274992164cc7affb702b17f1a88af8
Dmitriy <dn@monei.com>
2024-05-30 11:02:52 +0200

    add WooCommerce HPOS compatibility

--- woocommerce-gateway-monei.php
+++ woocommerce-gateway-monei.php
@@ -2,10 +2,10 @@
 /**
  * MONEI Payments for WooCommerce
  *
- * @package MONEI Payments
- * @author MONEI
+ * @package   MONEI Payments
+ * @author    MONEI
  * @copyright 2020-2020 MONEI
- * @license GPL-2.0+
+ * @license   GPL-2.0+
  *
  * Plugin Name: MONEI Payments for WooCommerce
  * Plugin URI: https://wordpress.org/plugins/monei/
@@ -25,16 +25,25 @@
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if ( !defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
 }
 
 define( 'MONEI_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'MONEI_PLUGIN_FILE', __FILE__ );
 
+/**
+ * Add compatibility with WooCommerce HPOS.
+ */
+add_action( 'before_woocommerce_init', function () {
+    if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+        \Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+    }
+} );
+
 require_once 'class-woocommerce-gateway-monei.php';
 function WC_Monei() {
-	return Woocommerce_Gateway_Monei::instance();
+    return Woocommerce_Gateway_Monei::instance();
 }
 
 WC_Monei();
