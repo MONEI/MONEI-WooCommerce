4bb5de09567ed7c141f17e73c7d2063c00d5ddab
malmo-monei <malmo@monei.com>
2024-10-14 11:42:04 +0200

    Create no apikey disabled notice

---
+++ templates/notice-admin-gateway-not-available-api.php
@@ -0,0 +1,8 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly 
+?>
+<div class="inline error">
+  <p>
+    <strong><?php esc_html_e('Gateway Disabled', 'monei'); ?></strong>: <?php esc_html_e('MONEI API key or Account ID is missing.', 'monei'); ?>
+  </p>
+</div>
