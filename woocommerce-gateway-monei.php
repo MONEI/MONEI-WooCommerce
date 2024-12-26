bd3ed53d517aee9138e2ac0f5c22451d6b002873
malmo-monei <malmo@monei.com>
2024-12-26 12:06:53 +0100

    Update woo tested version

--- woocommerce-gateway-monei.php
+++ woocommerce-gateway-monei.php
@@ -15,7 +15,7 @@
  * Author URI: https://www.monei.com/
  * Tested up to: 6.7
  * WC requires at least: 3.0
- * WC tested up to: 9.4
+ * WC tested up to: 9.5
  * Requires PHP: 7.2
  * Text Domain: monei
  * Domain Path: /languages/
