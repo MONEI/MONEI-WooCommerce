73a4d1ae9f88601eb91b7e10cb131868aa86af73
malmo-monei <malmo@monei.com>
2025-06-04 15:06:33 +0200

    Add 30 seconds caching

--- src/Repositories/PaymentMethodsRepository.php
+++ src/Repositories/PaymentMethodsRepository.php
@@ -6,33 +6,50 @@ use Monei\MoneiClient;
 
 class PaymentMethodsRepository implements PaymentMethodsRepositoryInterface {
 	private $accountId;
-    private MoneiClient $moneiClient;
+	private MoneiClient $moneiClient;
 
-    public function __construct( string $accountId, MoneiClient $moneiClient) {
-		$this->accountId = $accountId;
-        $this->moneiClient = $moneiClient;
+	public function __construct( string $accountId, MoneiClient $moneiClient ) {
+		$this->accountId   = $accountId;
+		$this->moneiClient = $moneiClient;
 	}
 
 	/**
 	 * Fetch payment methods from the API.
 	 */
 	private function fetchFromAPI(): ?array {
-        if ( ! $this->accountId ) {
-            return null;
-        }
-        try {
-            $response = $this->moneiClient->paymentMethods->get( $this->accountId );
-        } catch ( \Exception $e ) {
-            $response = null;
-        }
+		if ( ! $this->accountId ) {
+			return null;
+		}
+		try {
+			$response = $this->moneiClient->paymentMethods->get( $this->accountId );
+		} catch ( \Exception $e ) {
+			$response = null;
+		}
 
-		return json_decode($response, true);
+		return json_decode( $response, true );
 	}
 
 	/**
 	 * Get payment methods (fetch from transient or API).
 	 */
 	public function getPaymentMethods(): array {
-		return $this->fetchFromAPI() ?: array();
+		$transientKey = $this->generateTransientKey( $this->accountId );
+		$data         = get_transient( $transientKey );
+
+		if ( ! $data ) {
+			$data = $this->fetchFromAPI();
+			if ( $data ) {
+				set_transient( $transientKey, $data, 30 );
+			}
+		}
+
+		return $data ?: array();
+	}
+
+	/**
+	 * Generate a transient key.
+	 */
+	private function generateTransientKey( string $key ): string {
+		return 'payment_methods_' . md5( $key );
 	}
 }
