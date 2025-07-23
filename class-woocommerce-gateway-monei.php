99669219796ce6e948d6331f477b56c8d129cbe6
malmo-monei <malmo@monei.com>
2025-07-23 18:39:34 +0200

    Update version number

--- class-woocommerce-gateway-monei.php
+++ class-woocommerce-gateway-monei.php
@@ -5,7 +5,7 @@
  * @author   MONEI
  * @category Core
  * @package  Woocommerce_Gateway_Monei
- * @version  6.3.7
+ * @version  6.3.8
  */
 
 use Monei\Core\ContainerProvider;
@@ -25,7 +25,7 @@ if ( ! class_exists( 'Woocommerce_Gateway_Monei' ) ) :
 		 *
 		 * @var string
 		 */
-		public $version = '6.3.7';
+		public $version = '6.3.8';
 
 		/**
 		 * The single instance of the class.
@@ -324,7 +324,16 @@ if ( ! class_exists( 'Woocommerce_Gateway_Monei' ) ) :
 		 */
 		public function plugins_loaded() {
 			add_filter( 'woocommerce_payment_gateways', array( $this, 'add_gateways' ) );
-		}
+            add_filter('plugin_action_links_' . plugin_basename(__FILE__), array( $this, 'plugin_action_links' )  );
+
+
+        }
+        public function plugin_action_links( $links ) {
+
+                $links[] = '<a href="'. admin_url( 'admin.php?page=wc-settings&tab=monei_settings' ). '">'. __( 'Settings', 'woocommerce' ). '</a>';
+
+            return $links;
+        }
 
 		/**
 		 * Add Monei Gateways.
