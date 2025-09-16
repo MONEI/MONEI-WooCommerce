f6d85df9476ae2bd79d40e992431d9404c862768
malmo-monei <malmo@monei.com>
2025-09-16 14:45:17 +0200

    Update setUserAgent to include comment

--- src/Services/sdk/MoneiSdkClientFactory.php
+++ src/Services/sdk/MoneiSdkClientFactory.php
@@ -21,10 +21,14 @@ class MoneiSdkClientFactory {
 	public function get_client() {
 		if ( $this->client === null ) {
 			include_once WC_Monei()->plugin_path() . '/vendor/autoload.php';
-			$config = Configuration::getDefaultConfiguration();
+			$config       = Configuration::getDefaultConfiguration();
 			$this->client = new MoneiClient( $this->apiKeyService->get_api_key(), $config );
-            $this->client->setUserAgent( 'MONEI/WooCommerce/'. WC_Monei()->version  );
-		}
+			$this->client->setUserAgent(
+				'MONEI/WooCommerce/' . WC_Monei()->version .
+				' (WordPress v' . get_bloginfo( 'version' ) .
+				'; Woo v' . WC()->version .
+				'; PHP v' . phpversion() . ')'
+			);      }
 		return $this->client;
 	}
 }
